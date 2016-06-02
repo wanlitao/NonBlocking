c5daf84063fe3e71f9d781b177e7f3ed1eb39ab6
万里涛 <wanlitao_3055601@outlook.com>
2016-06-02 21:46:32 +0800

    add GetOrAdd concurrency test

---
+++ test/NonBlockingTests/ConcurrencyTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NonBlockingTests
+{
+    public class ConcurrencyTests
+    {
+        [Fact]
+        private void TestGetOrAdd_NonBlocking()
+        {
+            ValueFactory(null);
+            var nonBlocking_dict = new NonBlocking.ConcurrentDictionary<string, string>();
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 1000; i++)
+            {
+                Func<int, Task> task = async (j) =>
+                {
+                    await Task.Yield();
+                    var value = nonBlocking_dict.GetOrAdd("Test", _value);
+                    Assert.Equal(_value, value);
+                };
+
+                tasks.Add(task(i));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        [Fact]
+        private void TestGetOrAdd_System()
+        {
+            ValueFactory(null);
+            var dict = new System.Collections.Concurrent.ConcurrentDictionary<string, string>();
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 1000; i++)
+            {
+                Func<int, Task> task = async (j) =>
+                {
+                    await Task.Yield();
+                    var value = dict.GetOrAdd("Test", _value);
+                    Assert.Equal(_value, value);
+                };
+
+                tasks.Add(task(i));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private volatile string _value = null;
+
+        private string ValueFactory(string key)
+        {
+            return _value = Path.GetRandomFileName();
+        }
+    }
+}
